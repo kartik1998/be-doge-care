6e3294fca98e90d236b4698f17991f2503183426
kartik1998 <kartikrawat61@gmail.com>
2021-09-18 00:13:52 +0530

    Update ping route

--- app/index.js
+++ app/index.js
@@ -5,5 +5,5 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => res.status(200).send('AoK'));
+app.get('/ping', (_, res) => res.status(200).send('pong'));
 module.exports = app;
