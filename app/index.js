94cabaec1ce4a4d4ba745f1d503747b371e61265
kartik1998 <kartikrawat61@gmail.com>
2021-09-18 00:31:53 +0530

    Update index

--- app/index.js
+++ app/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const codes = require('./lib/statusCodes');
 
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.get('/ping', (_, res) => res.status(200).send('pong'));
+app.get('/ping', (_, res) => res.status(codes.SUCCESS).send('pong'));
 module.exports = app;
